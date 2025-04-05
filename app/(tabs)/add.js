151e4be1f7a631f20c825dd31baa8d93b86ebde9
PeterNguyen4 <peternguyen644@gmail.com>
2025-04-05 16:59:36 -0700

    Add scanning flag mechanism and redirect after one scan

--- app/(tabs)/add.js
+++ app/(tabs)/add.js
@@ -1,23 +1,41 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { CameraView, useCameraPermissions } from 'expo-camera';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, AppState } from 'react-native';
 import axios from 'axios';
+import { useRouter } from 'expo-router';
+
+const sendData = async (data) => {
+    try {
+      const response = await axios.get(`http://100.83.61.208:3001/test/${data}`);
+      Alert.alert("Success", response.data);
+    } catch (error) {
+      console.error("Error sending data:", error);
+      Alert.alert("Error", "Failed to send data to server");
+    }
+};
 
 export default function App() {
-    const sendData = async (data) => {
-        try {
-          const response = await axios.get(`http://100.83.61.208:3001/test/${data}`);
-          Alert.alert("Success", response.data);
-        } catch (error) {
-          console.error("Error sending data:", error);
-          Alert.alert("Error", "Failed to send data to server");
-        }
-    };
+    const router = useRouter();
+    const scannerState = useRef(false);
+    const appState = useRef(AppState.currentState);
     const [permission, requestPermission] = useCameraPermissions();
+
+    useEffect(() => {
+        const subscription = AppState.addEventListener("change", (event) => {
+        if (event === "active" && appState.current.match(/inactive|background/)) {
+            scannerState.current = false;
+        }
+        appState.current = event;
+    });
+
+    return () => {
+        subscription.remove();
+    };
+    }, []);
     
-  if (!permission) {
+    if (!permission) {
     return <View />;
-  }
+    }
 
   if (!permission.granted) {
     return (
@@ -30,27 +48,29 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-        {/* <Link href={'/home'} asChild> */}
-            <CameraView style={styles.camera} type='back' onBarcodeScanned={async (scanned) => {
-                console.log('!!!BARCODE ID:', scanned.data)
-                sendData(scanned.data)
-            }}>
-            </CameraView>
-        {/* </Link> */}
+        <CameraView style={styles.camera} type='back' onBarcodeScanned={async (barcodeData) => {
+                if (barcodeData && !scannerState.current) {
+                    scannerState.current = true;
+                    console.log('!!!BARCODE ID:', barcodeData.data)
+                    sendData(barcodeData.data)
+                    router.push('/home')
+                }
+            }} 
+        /> 
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  message: {
-    textAlign: 'center',
-    paddingBottom: 10,
-  },
-  camera: {
-    flex: 1,
-  }
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    message: {
+        textAlign: 'center',
+        paddingBottom: 10,
+    },
+    camera: {
+        flex: 1,
+    }
 });
