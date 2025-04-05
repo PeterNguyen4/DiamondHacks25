096c833b7a854bc1958b7b42b0c99245666b1cf2
bchoUCSD <bcho@ucsd.edu>
2025-04-05 13:01:56 -0700

    finished api route to get food item related to barcode

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = 3001;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/api/:productID', async (req, res) => {
+    try {
+        const productID = req.params.productID
+        const apiURL = `https://world.openfoodfacts.net/api/v2/product/${productID}?product_type=food&fields=product_name%2Cnutriments%2Cselected_images%2Cserving_quantity%2Cserving_quantity_unit`
+        const response = await axios.get(apiURL);
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error querying the API:', error.message);
+        res.status(500).json({ error: 'Failed to fetch data from the API' });
+    }
+});
+
+app.use((err, req, res, next) => {
+    console.error('Unhandled error:', err.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
